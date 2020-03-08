f54227a5773fcd1e24b469f766233eabcf138144
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-03-08 10:51:56 -0300

    Write first test

---
+++ api/tests/tests_views.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from api.models import Driver
+
+DRIVER_ENDPOINT = reverse('driver')
+
+
+class DriversTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.default_date = datetime(year=1996, month=6, day=26).astimezone()
+
+    def test_driver_register(self):
+        payload = {
+            'name': 'Joãozinho Truckpad',
+            'gender': 'M',
+            'birth_date': self.default_date,
+            'cnh_type': 'D',
+            'is_loaded': False,
+            'vehicle_type': 5
+        }
+
+        response = self.client.post(DRIVER_ENDPOINT, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Driver.objects.count(), 1)
