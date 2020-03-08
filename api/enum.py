b86fd96626efb013394229851e6c65c9c2905112
Tyrone Damasceno <tyronedamasceno@gmail.com>
2020-03-08 11:19:53 -0300

    Add Driver model

---
+++ api/enum.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+from enum import Enum
+
+choice = namedtuple('Choice', 'name value')
+
+
+class ChoiceEnum(Enum):
+    @classmethod
+    def choices(cls):
+        return [choice(e.name, e.value) for e in cls]
+
+
+class Gender(ChoiceEnum):
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
+
+
+class CnhType(ChoiceEnum):
+    A = 'A'
+    B = 'B'
+    C = 'C'
+    D = 'D'
+    E = 'E'
+
+
+class VehicleType(ChoiceEnum):
+    CAMINHAO_3_4 = 1
+    CAMINHAO_TOCO = 2
+    CAMINHAO_TRUCK = 3
+    CARRETA_SIMPLES = 4
+    CARRETA_EIXO_ESTENDIDO = 5
--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from api.enum import Gender, CnhType, VehicleType
+
+
+class Location(models.Model):
+    name = models.CharField('Nome da localidade', max_length=255)
+    latitude = models.DecimalField(
+        'Latitude', max_digits=10, decimal_places=7
+    )
+    longitude = models.DecimalField(
+        'Longitude', max_digits=10, decimal_places=7
+    )
+
+
+class Driver(models.Model):
+    name = models.CharField('Nome do motorista', max_length=255)
+    birth_date = models.DateTimeField('Data de nascimento')
+    gender = models.CharField('Sexo', max_length=10, choices=Gender.choices())
+    cnh_type = models.CharField(
+        'Categoria da CNH', max_length=2, choices=CnhType.choices()
+    )
+    is_loaded = models.BooleanField('Está carregado', default=False)
+    vehicle_type = models.IntegerField(
+        'Tipo do caminhão', choices=VehicleType.choices()
+    )
+    origin = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, related_name='drivers_origin',
+        null=True
+    )
+    destiny = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, related_name='drivers_destiny',
+        null=True
+    )
